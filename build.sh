11c8b5d81917837dc27e96058f52ae8ac9b03ad6
Mikko Nieminen <mikko.nieminen@bih-charite.de>
2024-09-19 17:06:57 +0200

    upgrade to irods v4.3.3 (#16)

--- build.sh
+++ build.sh
@@ -2,15 +2,20 @@
 
 export REPO=ghcr.io/bihealth/irods-docker
 export IRODS_PKG_VERSION=${IRODS_PKG_VERSION-4.3.1}
+export IRODS_PYTHON_RULE_ENGINE_VERSION=${IRODS_PYTHON_RULE_ENGINE_VERSION-4.3.1.0}
 export BUILD_VERSION=${BUILD_VERSION-1}
 
 docker build \
     -t "${REPO}:${IRODS_PKG_VERSION}-${BUILD_VERSION}" \
+    --build-arg IRODS_PKG_VERSION=${IRODS_PKG_VERSION} \
+    --build-arg IRODS_PYTHON_RULE_ENGINE_VERSION=${IRODS_PYTHON_RULE_ENGINE_VERSION} \
     --target main \
     docker
 
 docker build \
     -t "${REPO}:${IRODS_PKG_VERSION}-${BUILD_VERSION}-sssd" \
+    --build-arg IRODS_PKG_VERSION=${IRODS_PKG_VERSION} \
+    --build-arg IRODS_PYTHON_RULE_ENGINE_VERSION=${IRODS_PYTHON_RULE_ENGINE_VERSION} \
     --target sssd \
     docker
 
