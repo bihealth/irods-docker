e987ed8e1e5a98dca37f420f49affe9aaee8a51e
Federico Marotta <federico.marotta@bih-charite.de>
2026-08-26 13:34:15 +0200

    optionally purge systemd-timesyncd (#93)

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,6 +6,13 @@ set -euo pipefail
 
 if [[ "$1" == "irods-start" ]]; then
 
+    # Remove systemd-timesyncd due to a possible conflict between the irods
+    # service account and this package's service account uid (see #93)
+    if [[ "$PURGE_SYSTEMD_TIMESYNCD" == "1" ]] && id -u systemd-timesync 2> /dev/null; then
+        apt purge -y systemd-timesyncd
+        userdel systemd-timesync
+    fi
+
     # Set up service user and permissions
     groupadd -f -g $IRODS_SERVICE_ACCOUNT_GID $IRODS_SERVICE_ACCOUNT_GROUP
     useradd -d /var/lib/irods -s /bin/bash -u $IRODS_SERVICE_ACCOUNT_UID -g $IRODS_SERVICE_ACCOUNT_GID $IRODS_SERVICE_ACCOUNT_USER || true
